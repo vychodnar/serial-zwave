de3ba6ef15a0887cee705cb0d6deecdbe69df327
Michal Valovcik <valovcikm@gmail.com>
2016-12-10 15:05:20 -0500

    Adding some sample rabbitmq code

--- serial-adapter/bootstrap/bootstrap.py
+++ serial-adapter/bootstrap/bootstrap.py
@@ -39,8 +39,27 @@ def run():
     connection.close()
 
 
+def callback(ch, method, properties, body):
+    print(" [x] %r" % body)
+
+@click.command()
+def rec():
+    """Receive message"""
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.exchange_declare(exchange='logs', type='fanout')
+    result = channel.queue_declare(exclusive=True)
+    queue_name = result.method.queue
+    channel.queue_bind(exchange='logs', queue=queue_name)
+    print(' [*] Waiting for logs. To exit press CTRL+C')
+    channel.basic_consume(callback, queue=queue_name, no_ack=True)
+    channel.start_consuming()
+
+
+
 main.add_command(hello)
 main.add_command(run)
+main.add_command(rec)
 
 
 class Boo(Stuff):
