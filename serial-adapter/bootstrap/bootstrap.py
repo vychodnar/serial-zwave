752bede5ebe6eb25432e56fefe87cbadab5783cd
Michal Valovcik <valovcikm@gmail.com>
2016-12-10 14:46:36 -0500

    Adding new command for posting a message to a queue

--- serial-adapter/bootstrap/bootstrap.py
+++ serial-adapter/bootstrap/bootstrap.py
@@ -2,12 +2,16 @@
 
 import sys
 import click
+import pika
 from .stuff import Stuff
 
 __version__ = "0.0.1"
 
+@click.version_option(version=__version__)
+
 @click.group()
 def main():
+    """Testor"""
     pass
 #    print("Executing bootstrap version %s." % __version__)
 #    print("List of argument strings: %s" % sys.argv[1:])
@@ -15,12 +19,29 @@ def main():
 
 @click.command()
 def hello():
+    """Hello World command"""
     print("Hello World!")
     print("Executing bootstrap version %s." % __version__)
     print("List of argument strings: %s" % sys.argv[1:])
     print("Stuff and Boo():\n%s\n%s" % (Stuff, Boo()))
 
+
+@click.command()
+def run():
+    """Run the command"""
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
+    channel = connection.channel()
+    channel.exchange_declare(exchange='logs', type='fanout')
+    #message = ' '.join(sys.argv[1:])
+    message = "info: Hello World!"
+    channel.basic_publish(exchange='logs', routing_key='', body=message)
+    print(" [x] Sent %r" % message)
+    connection.close()
+
+
 main.add_command(hello)
+main.add_command(run)
+
 
 class Boo(Stuff):
     pass
---
+++ serial-adapter/bootstrap_runner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Convenience wrapper for running bootstrap directly from source tree."""
+
+
+from bootstrap.bootstrap import main
+
+
+if __name__ == '__main__':
+    main()
+    
